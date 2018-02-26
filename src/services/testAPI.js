328a941225bf3d793a779620ca11303cb668209d
mervin <mydia@foxmail.com>
2018-02-26 14:44:09 +0800

    update mock datas

--- src/services/testAPI.js
+++ src/services/testAPI.js
@@ -1,8 +1,11 @@
 export default {
   "apiIn": `$m.obj({
-    data: $m.arr(1,30,'rint',1 ,30)
+    data: $m.arr(1,30,"{name: $m.arr(1,30,'cstr',1)}")
   })`,
   "apiIn2": `$m.obj({
+    data: $m.arr(1,30,{name: "tom")})
+  })`,
+  "apiIn3": `$m.obj({
     data: [{
       delegateName: $m.cstr(1,10),
       delegatelist_child: [{
@@ -14,7 +17,7 @@ export default {
       }]
     }]
   })`,
-  "apiIn3": `$m.obj({
+  "apiIn4": `$m.obj({
     data:$m.arr(1,30,'str_low',1)
   })`,
 }
